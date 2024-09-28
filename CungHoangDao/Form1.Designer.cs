6c11bdc413636090315069d2a221f9a0dff66e61
vansonnguyen2209 <vansonnguyen2209@gmail.com>
2024-09-29 00:06:11 +0700

    bai04

---
+++ CungHoangDao/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace CungHoangDao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox2.Location = new System.Drawing.Point(200, 51);
+            this.maskedTextBox2.Mask = "00/00/0000";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(105, 30);
+            this.maskedTextBox2.TabIndex = 1;
+            this.maskedTextBox2.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox2_MaskInputRejected);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(62, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ngày sinh";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(200, 124);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(105, 30);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button1.Location = new System.Drawing.Point(67, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Xem ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button2.Location = new System.Drawing.Point(67, 160);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 37);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Xóa";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 225);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ CungHoangDao/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CungHoangDao
+{
+    public partial class Form1 : Form
+    {
+        int x, z, y = 0;
+        string datestr = "";
+        string[] str;
+        string day, month, year;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            datestr = maskedTextBox2.Text;
+            if(datestr == "  /  /")
+            {
+                MessageBox.Show("Vui long nhap ngay sinh");
+            }
+            else
+            {
+
+            str = datestr.Split('/');
+            day = str[0];
+            month = str[1];
+            year = str[2];
+            x = Convert.ToInt32(day);
+            y = Convert.ToInt32(month);
+            z = Convert.ToInt32(year);
+
+            if (z > 0 && z < 2025)
+            {
+                switch (y)
+                {
+                    case 1:
+                        {
+                            if (x > 0 && x <= 20)
+                            {
+                                textBox1.Text = "Ma Kết";
+                            }
+                            else if (x > 20 && x <= 31)
+
+                            {
+                                textBox1.Text = "Bảo Bình";
+                            }
+                            else
+                            {
+                                MessageBox.Show("Ngay sinh khong hop le !");
+                                maskedTextBox2.Clear();
+                                break;
+                            }
+                        break;
+                        }
+                    case 2:
+                        {
+                            if (x > 0 && x <= 19)
+                            {
+                                textBox1.Text = "Bảo Bình";
+                            }
+                            else if (x > 19 && x <= 28)
+
+                            {
+                                textBox1.Text = "Song Ngư";
+                            }
+                            else if(x == 29 && ((z % 4 == 0 && z % 100 != 0) || z % 400 == 0))    
+                            {
+                                textBox1.Text = "Song Ngư";
+                            }
+                            else
+                            {
+                                MessageBox.Show("Ngay sinh khong hop le !");
+                                maskedTextBox2.Clear();
+                                break;
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            if (x > 0 && x <= 20)
+                            {
+                                textBox1.Text = "Song ngư";
+                            }
+                            else if (x > 20 && x <= 31)
+
+                            {
+                                textBox1.Text = "Bạch Dương";
+                            }
+                            else
+                            {
+                                MessageBox.Show("Ngay sinh khong hop le !");
+                                maskedTextBox2.Clear();
+                                break;
+                            }
+                            break;
+                        }
+                    case 4:
+                        {
+                            if (x > 0 && x <= 20)
+                            {
+                                textBox1.Text = "Bạch Dương";
+                            }
+                            else if (x > 20 && x <= 30)
+
+                            {
+                                textBox1.Text = "Kim Ngưu";
+                            }
+                            else
+                            {
+                                MessageBox.Show("Ngay sinh khong hop le !");
+                                maskedTextBox2.Clear();
+                                break;
+                            }
+                            break;
+                        }
+                    case 5:
+                        {
+                            if (x > 0 && x <= 21)
+                            {
+                                textBox1.Text = "Kim Ngưu";
+                            }
+                            else if (x > 21 && x <= 31)
+
+                            {
+                                textBox1.Text = "Song Tử";
+                            }
+                            else
+                            {
+                                MessageBox.Show("Ngay sinh khong hop le !");
+                                maskedTextBox2.Clear();
+                                break;
+                            }
+                            break;
+                        }
+                    case 6:
+                        {
+                            if (x > 0 && x <= 21)
+                            {
+                                textBox1.Text = "Song Tử";
+                            }
+                            else if (x > 21 && x <= 30)
+
+                            {
+                                textBox1.Text = "Cự Giải";
+                            }
+                            else
+                            {
+                                MessageBox.Show("Ngay sinh khong hop le !");
+                                maskedTextBox2.Clear();
+                                break;
+                            }
+                            break;
+                        }
+                    case 7:
+                        {
+                            if (x > 0 && x <= 22)
+                            {
+                                textBox1.Text = "Cự Giải";                           
+                            }
+                            else if (x > 22 && x <= 31)
+
+                            {
+                                textBox1.Text = "Sư Tử";
+                            }
+                            else
+                            {
+                                MessageBox.Show("Ngay sinh khong hop le !");
+                                maskedTextBox2.Clear();
+                                break;
+                            }
+                            break;
+                        }
+                    case 8:
+                        {
+                            if (x > 0 && x <= 22)
+                            {                              
+                                textBox1.Text = "Sư Tử";
+                            }
+                            else if (x > 22 && x <= 31)
+
+                            {
+                                textBox1.Text = "Xử Nữ";
+                            }
+                            else
+                            {
+                                MessageBox.Show("Ngay sinh khong hop le !");
+                                maskedTextBox2.Clear();
+                                break;
+                            }
+                            break;
+                        }
+                    case 9:
+                        {
+                            if (x > 0 && x <= 23)
+                            {
+                                textBox1.Text = "Xử Nữ";
+                            }
+                            else if (x > 23 && x <= 30)
+
+                            {
+                                textBox1.Text = "Thiên Bình";
+                            }
+                            else
+                            {
+                                MessageBox.Show("Ngay sinh khong hop le !");
+                                maskedTextBox2.Clear();
+                                break;
+                            }
+                            break;
+                        }
+                    case 10:
+                        {
+                            if (x > 0 && x <= 23)
+                            {
+                                textBox1.Text = "Thiên Bình";
+                            }
+                            else if (x > 23 && x <= 31)
+
+                            {
+                                textBox1.Text = "Thân Nông";
+                            }
+                            else
+                            {
+                                MessageBox.Show("Ngay sinh khong hop le !");
+                                maskedTextBox2.Clear();
+                                break;
+                            }
+                            break;
+                        }
+                    case 11:
+                        {
+                            if (x > 0 && x <= 22)
+                            {
+                                textBox1.Text = "Thần Nông";
+                            }
+                            else if (x > 22 && x <= 30)
+
+                            {
+                                textBox1.Text = "Nhân Mã";
+                            }
+                            else
+                            {
+                                MessageBox.Show("Ngay sinh khong hop le !");
+                                maskedTextBox2.Clear();
+                                break;
+                            }
+                            break;
+                        }
+                    case 12:
+                        {
+                            if (x > 0 && x <= 21)
+                            {
+                                textBox1.Text = "Nhân Mã";
+                            }
+                            else if (x > 21 && x <= 31)
+
+                            {
+                                textBox1.Text = "Ma Kết";
+                            }
+                            else
+                            {
+                                MessageBox.Show("Ngay sinh khong hop le !");
+                                maskedTextBox2.Clear();
+                                break;
+                            }
+                            break;
+                        }
+                     default :
+                                MessageBox.Show("Ngay sinh khong hop le !");
+                            maskedTextBox2.Clear();
+                            break;
+
+                        }
+                }
+
+
+            else
+            {
+                MessageBox.Show("Ngay sinh khong hop le !");
+
+            }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            maskedTextBox2.Clear();
+            textBox1.Clear();
+            datestr = " / / ";
+        }
+
+        private void maskedTextBox2_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+ 
+        
+    }
+}
