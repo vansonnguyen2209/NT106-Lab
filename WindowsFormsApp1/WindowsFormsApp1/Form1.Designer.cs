4676d30130443d304038c7b7f249a9be004c9095
vansonnguyen2209 <vansonnguyen2209@gmail.com>
2024-10-07 21:41:28 +0700

    first

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(377, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(712, 583);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(376, 19);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(712, 583);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.treeView1.Location = new System.Drawing.Point(12, 19);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(346, 583);
+            this.treeView1.TabIndex = 7;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1106, 619);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Bai05";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Net.WebRequestMethods;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (treeView1.SelectedNode != null)
+            {
+
+                TreeNode selectedNode = treeView1.SelectedNode;
+                string selectedPath =  selectedNode.FullPath;
+                switch(Path.GetExtension(selectedPath))
+                {
+                    case ".txt":
+                    case ".doc":
+                    case ".docx":
+                        richTextBox1.BringToFront();
+                        using (FileStream file = System.IO.File.Open(Path.GetFullPath(selectedPath), FileMode.Open))
+                        {
+                            StreamReader text = new StreamReader(file);
+                            this.richTextBox1.Text = text.ReadToEnd();
+                        }
+                        break;
+                    case ".png":
+                    case ".ico":
+                    case ".jpg":
+                        pictureBox1.BringToFront();
+                        pictureBox1.Load(selectedPath);
+                        break;
+                    default:
+                        richTextBox1.Clear();
+                        pictureBox1.Image = null;
+                        break;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] drives = Environment.GetLogicalDrives();
+            foreach (string drive in drives)
+            {
+                DriveInfo driveinfo = new DriveInfo(drive);
+                if (driveinfo.IsReady)
+                {
+                    TreeNode root = new TreeNode(driveinfo.Name);
+                    treeView1.Nodes.Add(root);
+
+                    var folders = new DirectoryInfo(drive).GetDirectories();
+                    foreach(DirectoryInfo folder in folders)
+                    {
+                        if (Directory.Exists(folder.FullName))
+                        {
+                            TreeNode node = new TreeNode(folder.Name);
+                            root.Nodes.Add(node);
+                            LoadFolder(node);
+                            LoadFile(node);
+                        }
+                    }
+                }
+            }
+
+
+
+        }
+        private void LoadFolder(TreeNode root)
+        {
+            if (root == null) return;
+            try
+            {
+                var folders = new DirectoryInfo(root.FullPath).GetDirectories();
+                if (folders.Count() == 0) return;    
+                foreach (DirectoryInfo folder in folders)
+                {
+                    if (Directory.Exists(folder.FullName)) {
+                        TreeNode node = new TreeNode(folder.Name);
+                        root.Nodes.Add(node);
+                        LoadFolder(node);
+                    }
+                }
+            }
+            catch
+            {
+                return;
+            };
+
+
+        }
+
+
+            private void LoadFile(TreeNode root)
+        {
+            if (root == null) return;
+            try
+            {
+                var files = new DirectoryInfo(root.FullPath).GetFiles();
+                if (files.Count() == 0) return;
+                foreach (FileInfo file in files)
+                {
+                    if (System.IO.File.Exists(file.FullName))
+                    {
+                        TreeNode node = new TreeNode(file.Name);
+                        root.Nodes.Add(node);
+                        LoadFile(node);
+                    }
+                }
+            }
+            catch
+            {
+                return;
+            };
+        }
+    }
+}
